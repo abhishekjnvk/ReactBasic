dfd341c46577c011e4506b1eed88627c562efa8e
Abhishek Kumar <abhishekkumarjnvk@gmail.com>
2020-05-11 22:51:10 +0530

    first commit

---
+++ precache-manifest.bfe2cb79c5161d3652cb7bae75affa8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c328d61c8181ab910930c8aba56f3b",
+    "url": "/ReactBasic/index.html"
+  },
+  {
+    "revision": "99a9c3f35d252be00307",
+    "url": "/ReactBasic/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4b948fbf551667576e86",
+    "url": "/ReactBasic/static/css/main.5b759d0b.chunk.css"
+  },
+  {
+    "revision": "99a9c3f35d252be00307",
+    "url": "/ReactBasic/static/js/2.662b13f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactBasic/static/js/2.662b13f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b948fbf551667576e86",
+    "url": "/ReactBasic/static/js/main.ee0b66cd.chunk.js"
+  },
+  {
+    "revision": "9cca8c6408afb903482a",
+    "url": "/ReactBasic/static/js/runtime-main.e909c7a7.js"
+  }
+]);
